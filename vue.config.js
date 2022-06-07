cf3df274236b51a687aa64d38fea8e7feedbb6dd
porfinogeneta <szyman123drum@gmail.com>
2022-06-07 17:00:57 +0200

    Icon changed.

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,14 @@ module.exports = defineConfig({
       : '/',
   pwa: {
     name: "ViewTube",
-    themeColor: "#3d3c3c"
+    themeColor: "#3d3c3c",
+    icons: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
   }
 
 })
