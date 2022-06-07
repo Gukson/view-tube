117763bd45913d0f548a8a7de8a127e91641e8d9
porfinogeneta <szyman123drum@gmail.com>
2022-06-07 12:47:30 +0200

    Installation improvements.

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === "development" ? "/view-tube/" : "",
-  // configureWebpack: {
-  //   plugins: [new GenerateSW()]
-  // }
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/view-tube/'
+      : '/',
   pwa: {
     name: "ViewTube",
     themeColor: "#3d3c3c"
