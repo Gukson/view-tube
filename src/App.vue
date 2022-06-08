d8f0dbdb8da21c3c2d5e9b96e1aede57be1edf11
porfinogeneta <szyman123drum@gmail.com>
2022-06-08 17:36:02 +0200

    UI minor improvements.

--- src/App.vue
+++ src/App.vue
@@ -46,23 +46,11 @@ body {
   background-color: rgb(30 41 59);
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
